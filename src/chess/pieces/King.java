3cec4341ce0efc5dfa4abf2f8fa0ef7fcc1692d6
PedroLeonardo8 <pedroleonardo841@gmail.com>
2020-08-06 11:52:02 -0300

    Pecas no tabuleiro: Torre e Rei

---
+++ src/chess/pieces/King.java
@@ -0,0 +1,18 @@
+package chess.pieces;
+
+import chess.ChessPiece;
+import chess.Color;
+import tabuleiro.Board;
+
+public class King extends ChessPiece{
+
+	public King(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "R";
+	}
+
+}
