d7c802337030084ab5359f049bfb82b980ef8b3a
PedroLeonardo8 <pedroleonardo841@gmail.com>
2020-08-11 10:06:44 -0300

    Mostrar movimentos possiveis

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,10 +1,10 @@
 package chess;
 
+import chess.pieces.King;
+import chess.pieces.Rook;
 import tabuleiro.Board;
 import tabuleiro.Piece;
 import tabuleiro.Position;
-import chess.pieces.King;
-import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -23,6 +23,12 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+
+	public boolean[][] possibleMoves (ChessPosition sourcePosition){
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
 	}
 
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
